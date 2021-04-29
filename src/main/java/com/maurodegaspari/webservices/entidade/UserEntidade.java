1821c89c309edb04620044d733e5b53213c71e87
maurodegaspari <mauro_degaspari@hotmail.com>
2021-04-29 12:04:42 -0300

    Implementação de Banco H2 para testes

--- src/main/java/com/maurodegaspari/webservices/entidade/UserEntidade.java
+++ src/main/java/com/maurodegaspari/webservices/entidade/UserEntidade.java
@@ -2,12 +2,21 @@ package com.maurodegaspari.webservices.entidade;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UserEntidade implements Serializable {
 	
 	/**
 	 * Numero de serie do User
 	 */
+	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id ;
 	private String nome;
 	private String email;
