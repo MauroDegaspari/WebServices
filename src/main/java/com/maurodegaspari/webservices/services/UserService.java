b5ff6aca9ce9d96aeed5583c08b2cd950315ae0e
maurodegaspari <mauro_degaspari@hotmail.com>
2021-04-29 11:40:56 -0300

    Package entidade, com UserEntidade

--- src/main/java/com/maurodegaspari/webservices/services/UserService.java
+++ src/main/java/com/maurodegaspari/webservices/services/UserService.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maurodegaspari.webservices.entidade.User;
+import com.maurodegaspari.webservices.entidade.UserEntidade;
 @RestController
 @RequestMapping(value = "/users")
 public class UserService {
 	
 		@GetMapping
-		public ResponseEntity<User> findAll() {
-			User usuario = new User(1L, "mauri", "mauro@hotmai.com", " 123526", "234");
+		public ResponseEntity<UserEntidade> findAll() {
+			UserEntidade usuario = new UserEntidade(1L, "mauri", "mauro@hotmai.com", " 123526", "234");
 			return ResponseEntity.ok().body(usuario);
 		}
 	
